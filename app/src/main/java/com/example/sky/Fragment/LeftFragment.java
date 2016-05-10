75516a463e0ed56f761aa8740a9da72d18c64bf0
chenyp <296546867@qq.com>
2016-05-10 17:34:45 +0800

    完善用户中心模块

--- app/src/main/java/com/example/sky/Fragment/LeftFragment.java
+++ app/src/main/java/com/example/sky/Fragment/LeftFragment.java
@@ -5,11 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -17,8 +15,7 @@ import android.widget.Toast;
 
 import com.example.sky.Activity.CenterActivity;
 import com.example.sky.Activity.R;
-import com.example.sky.Utils.ActivityCollector;
-import com.example.sky.adapter.LeftMenuListAdapter;
+import com.example.sky.MyAdapter.LeftMenuListAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
