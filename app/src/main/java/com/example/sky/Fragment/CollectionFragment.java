c869d2d9b83fc630acbe2e289affcb8b10806523
chenyp <296546867@qq.com>
2016-05-31 17:33:05 +0800

    1/足迹 2/收藏

--- app/src/main/java/com/example/sky/Fragment/CollectionFragment.java
+++ app/src/main/java/com/example/sky/Fragment/CollectionFragment.java
@@ -2,21 +2,37 @@ package com.example.sky.Fragment;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
 
 import com.example.sky.Activity.R;
+import com.example.sky.Bean.Article;
 import com.example.sky.Bean.CollectionItem;
+import com.example.sky.Bean.CollectionList;
+import com.example.sky.DataBase.DBManager;
+import com.example.sky.MyAdapter.CollectionGridViewAdapter;
 import com.example.sky.MyAdapter.MyAdapter;
+import com.example.sky.Net.Configurator;
+import com.example.sky.Utils.LoaddingDialog;
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
 
 /**
  * 项目名称：NewMagazine
@@ -27,64 +43,92 @@ import java.util.ArrayList;
  * 修改时间：2016/5/6 10:04
  * 修改备注：
  */
-public class CollectionFragment extends Fragment {
+public class CollectionFragment extends Fragment implements View.OnClickListener{
 
 
-    private Context mContext;
-    private GridView grid_photo;
-    private BaseAdapter mAdapter = null;
-    private ArrayList<CollectionItem> mData = null;
+    private GridView grid_collection;
+    CollectionList collectionList;//保存收藏的文章信息
+    Button collection_delete;//删除按钮
 
-
+    DBManager dbManager;
+    LoaddingDialog loaddingDialog;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.collectionfragment_context, container, false);
 
-
-
-        setGridView(view);
-
+        binViews(view);
+        setListener();
+        init();
         return view;
     }
 
-    private void  setGridView(View view){
+    private void  binViews(View view){
+        grid_collection = (GridView)view.findViewById(R.id.collection_gridview);
+        collection_delete = (Button)view.findViewById(R.id.collection_delete);
+    }
+    private void setListener(){
+        collection_delete.setOnClickListener(this);
+    }
+    private void init(){
+        dbManager = new DBManager();
+        //实例化loadding
+        loaddingDialog=new LoaddingDialog(getActivity());
+        loaddingDialog.setCanceledOnTouchOutside(false);// 设置点击屏幕Dialog不消失
+        //获取收藏数据，设置界面
+        GetCollectionData();
+    }
+    //获取杂志文章收藏数据
+    private void GetCollectionData(){
+        loaddingDialog.show();
+        OkHttpUtils
+                .get()
+                .url(Configurator.CollectionShow+"?token="+dbManager.readAccessToken())
+                .tag(getActivity())
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e) {
+//                        Toast.makeText(getActivity(), "网络异常,请稍后再试", Toast.LENGTH_SHORT).show();
+                        loaddingDialog.dismiss();
+                    }
 
-        grid_photo = (GridView) view.findViewById(R.id.collection_gridview);
-
-
-        mData = new ArrayList<CollectionItem>();
-        mData.add(new CollectionItem(R.mipmap.women,R.mipmap.close_btn,"helloworld","99"));
-        mData.add(new CollectionItem(R.mipmap.women,R.mipmap.close_btn,"helloworld","993"));
-        mData.add(new CollectionItem(R.mipmap.women,R.mipmap.close_btn,"helloworld","1"));
-        mData.add(new CollectionItem(R.mipmap.women,R.mipmap.close_btn,"helloworld","3"));
-        mData.add(new CollectionItem(R.mipmap.women,R.mipmap.close_btn,"helloworld","44"));
-        mData.add(new CollectionItem(R.mipmap.women,R.mipmap.close_btn,"helloworld","95"));
-        mData.add(new CollectionItem(R.mipmap.women,R.mipmap.close_btn,"helloworld","88"));
-
-
-        mAdapter = new MyAdapter<CollectionItem>(mData, R.layout.collection_gridview_item_layout) {
-            @Override
-            public void bindView(MyAdapter.ViewHolder holder, CollectionItem obj) {
-                holder.setImageResource(R.id.img_icon, obj.getArticleIMG());
-                holder.setImageResource(R.id.img_icon_delete, obj.getDeleteIMG());
-                holder.setText(R.id.txt_article_name, obj.getArticleName());
-                holder.setText(R.id.txt_article_collect_num, obj.getArticleCollectionNUM());
-
-            }
-        };
-
-        grid_photo.setAdapter(mAdapter);
-
-
-        grid_photo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getActivity(), "你点击了~" + position + "~项", Toast.LENGTH_SHORT).show();
-            }
-        });
-
+                    @Override
+                    public void onResponse(String s) {
+                        Log.i("myInfo",s);
+                        //解析数据
+                        collectionList = new Gson().fromJson(s,CollectionList.class);
+                        //设置适配器
+                        grid_collection.setAdapter(new CollectionGridViewAdapter(collectionList.getObj(),getActivity(),dbManager));
+                        loaddingDialog.dismiss();
+                    }
+                });
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.collection_delete:
+                Intent intent =  new Intent("com.chen.mybcreceiver.DeleteColltion");
+                Bundle bundle = new Bundle();
+                if(collection_delete.getText().toString().equals("完成")){
+                    bundle.putString("delete","true");
+                    collection_delete.setText("删除收藏");
+                }else{
+                    bundle.putString("delete","false");
+                    collection_delete.setText("完成");
+                }
+                intent.putExtras(bundle);
+                //发送广播，显示删除收藏文章的图标
+                LocalBroadcastManager.getInstance(getActivity()).sendBroadcast(intent);
 
+                break;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+    }
 }
---
+++ app/src/main/java/com/example/sky/MyAdapter/CollectionGridViewAdapter.java
@@ -0,0 +1,175 @@
+package com.example.sky.MyAdapter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.sky.Activity.R;
+import com.example.sky.Bean.Article;
+import com.example.sky.Bean.Result;
+import com.example.sky.DataBase.DBManager;
+import com.example.sky.Net.Configurator;
+import com.example.sky.Utils.LoaddingDialog;
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.util.List;
+
+import okhttp3.Call;
+
+/**
+ * 项目名称：NewMagazine
+ * 类描述：
+ * 创建人：meigong
+ * 创建时间：2016/5/31 15:05
+ * 修改人：meigong
+ * 修改时间：2016/5/31 15:05
+ * 修改备注：
+ */
+public class CollectionGridViewAdapter extends BaseAdapter {
+
+    List<Article> articleList;// 文章收藏列表
+    Context context;
+    DBManager dbManager;
+    LoaddingDialog loaddingDialog;
+
+    public CollectionGridViewAdapter(List<Article> articleList,Context context,DBManager dbManager){
+        this.articleList=articleList;
+        this.context=context;
+        this.dbManager=dbManager;
+
+        //实例化loadding
+        loaddingDialog=new LoaddingDialog(context);
+        loaddingDialog.setCanceledOnTouchOutside(false);// 设置点击屏幕Dialog不消失
+    }
+    @Override
+    public int getCount() {
+        return articleList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return articleList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        GridViewViewHolder holder = null;
+        if (convertView==null){
+            holder = new GridViewViewHolder();
+            convertView = LayoutInflater.from(context).inflate(R.layout.collection_gridview_item_layout,null);
+            holder.collection_item_img = (ImageView)convertView.findViewById(R.id.collection_item_img);
+            holder.collection_item_delete = (ImageView)convertView.findViewById(R.id.collection_item_delete);
+            holder.collection_item_title = (TextView)convertView.findViewById(R.id.collection_item_title);
+            holder.collection_item_collectionnum = (TextView)convertView.findViewById(R.id.collection_item_collectionnum);
+            convertView.setTag(holder);
+        }else {
+            holder = (GridViewViewHolder)convertView.getTag();
+        }
+        holder.collection_item_img.setImageResource(R.mipmap.empty_photo);
+        holder.collection_item_delete.setImageResource(R.mipmap.delete);
+        holder.collection_item_title.setText(articleList.get(position).getAtitle());
+        holder.collection_item_collectionnum.setText(articleList.get(position).getCollectnum());
+        //删除图片点击事件
+        holder.collection_item_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(context)
+                        .setMessage("删除该收藏文章？")
+                        .setNegativeButton("取消",null)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //loadding
+                                loaddingDialog.show();
+                                //删除数据
+                                deleteCollection(position);
+                            }
+                        }).create().show();
+            }
+        });
+
+        //注册广播,显示删除的图标
+        LocalBroadcastManager.getInstance(context).registerReceiver( new MyBRReceiver(holder), new IntentFilter("com.chen.mybcreceiver.DeleteColltion"));
+
+        return convertView;
+    }
+    private class GridViewViewHolder{
+        ImageView collection_item_img;//封面图
+        ImageView collection_item_delete;//删除
+        TextView collection_item_title;//标题
+        TextView collection_item_collectionnum;//简介
+    }
+
+    //删除收藏文章
+    private void deleteCollection(final int position){
+        OkHttpUtils
+                .post()
+                .url(Configurator.DeleteCollection)
+                .addParams("token",dbManager.readAccessToken())
+                .addParams("oid",articleList.get(position).getHid())
+                .tag(context)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e) {
+                        loaddingDialog.dismiss();
+                    }
+
+                    @Override
+                    public void onResponse(String s) {
+                        Result result = new Gson().fromJson(s,Result.class);
+                        if (result.getCode().equals("100")){
+                            //提示
+                            Toast.makeText(context, result.getMsg(), Toast.LENGTH_SHORT).show();
+                            //删除对应的文章数据
+                            articleList.remove(position);
+                            //刷新界面
+                            notifyDataSetChanged();
+                            //loadding
+                            loaddingDialog.dismiss();
+                        }
+                    }
+                });
+    }
+
+    /**
+     * 广播要做的事情，显示删除图标
+     */
+    public class MyBRReceiver extends BroadcastReceiver {
+        GridViewViewHolder gridViewViewHolder;
+        public MyBRReceiver(GridViewViewHolder gridViewViewHolder){
+            this.gridViewViewHolder=gridViewViewHolder;
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //获取传递的数据
+            Bundle bundle = intent.getExtras();
+            //判断是否显示删除图标
+            if (bundle.getString("delete").equals("true")){
+                gridViewViewHolder.collection_item_delete.setVisibility(View.GONE);
+            }else {
+                gridViewViewHolder.collection_item_delete.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+}
